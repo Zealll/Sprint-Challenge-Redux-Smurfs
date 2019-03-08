af15b8e6082fc33606217890a8e30003b3a5f201
unknown <eriznis@gmail.com>
2019-03-08 12:38:29 -0500

    added SmurfForm component, and a functionality to add a new object

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import { getSmurfs } from '../actions'
 import { connect } from 'react-redux'
 import Smurf from './Smurf'
+import SmurfForm from './SmurfForm'
 /*
  to wire this component up you're going to need a few things.
  I'll let you do this part on your own. 
@@ -18,6 +19,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <SmurfForm />
         {this.props.smurfs.map(smurf => {
           return <Smurf smurf={smurf} key={smurf.name} />
         })}
---
+++ smurfs/src/components/SmurfForm.js
@@ -0,0 +1,60 @@
+import React from 'react'
+import { connect } from 'react-redux'
+import { addSmurf } from '../actions'
+
+class SmurfForm extends React.Component {
+    state = {
+        name: '',
+        age: '',
+        height: ''
+    }
+
+    handleChange = e => {
+        e.preventDefault()
+        this.setState({ 
+            [e.target.name]: e.target.value
+        })
+    }
+
+    addButton = e => {
+        e.preventDefault()
+        this.props.addSmurf(this.state)
+        this.setState({
+            name: '',
+            age: '',
+            height: ''
+        })
+    }
+
+    render() {
+        return (
+            <form onSubmit={this.addButton}>
+                <input 
+                    name='name'
+                    value={this.state.name}
+                    type='text'
+                    placeholder='Name'
+                    onChange={this.handleChange}
+                />
+                <input 
+                    name='age'
+                    value={this.state.age}
+                    type='number'
+                    placeholder='Age'
+                    onChange={this.handleChange}
+                />
+                <input 
+                    name='height'
+                    value={this.state.height}
+                    type='number'
+                    placeholder='Height'
+                    onChange={this.handleChange}
+                />
+                
+                <button>Add Smurf</button>
+            </form>
+        )
+    }
+}
+
+export default connect(null, { addSmurf })(SmurfForm)
